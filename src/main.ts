6933cb6bb7d27a773eef0f68bb6099a1d8a78880
moon <ahmedabozaid.dev@gmail.com>
2026-01-09 11:38:43 +0200

    fixes prisma setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,16 @@ async function bootstrap() {
       },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('My Auth API')
+    .setDescription('The signup API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,23 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'src/generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const dbUrl = process.env.DATABASE_URL;
+    console.log('DATABASE_URL:', dbUrl);
+    if (!dbUrl) {
+      throw new Error('Missing required environment variable: DATABASE_URL');
+    }
+
+    const adapter = new PrismaMariaDb(dbUrl);
+
+    super({
+      adapter,
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
