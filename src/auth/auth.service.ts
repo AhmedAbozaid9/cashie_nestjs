e102c67062b510585c259dfe740b620e36c8ec36
moon <ahmedabozaid.dev@gmail.com>
2026-01-09 11:06:53 +0200

    adds sign up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignupDto } from './dto/signup.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup(dto: SignupDto) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          name: dto.name,
+          email: dto.email,
+          password: hashedPassword,
+          accounts: {
+            create: [{ name: 'Main', amount: 0 }],
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          plan: true,
+          accounts: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Email already in use');
+    }
+  }
+}
