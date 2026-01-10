faf6901ceaf74aa72a5bf40a3da998574eb79061
ahmed <ahmedabozaid.dev@gmail.com>
2026-01-10 13:49:17 +0200

    adds get user decorator

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
 
 @Controller('account')
 @UseGuards(JwtAuthGuard)
 export class AccountController {
   @Get()
-  getAccountInfo() {
-    return { message: 'This is a protected account route' };
+  getAccountInfo(@GetUser() userId: number) {
+    return { userId };
   }
 }
